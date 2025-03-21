f60fd498bf0e177fbae4ffebed1775080b71ba66
Madelyn Romero <m.romero013@gmail.com>
2025-03-21 14:44:13 -0600

    Add spec for Artist.average_song_length

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe Artist, type: :model do
     describe "relationships" do
         it { should have_many :songs }
     end
+
+    describe "instance methods" do
+        it ".average_song_length" do
+            artist_1 = Artist.create(name: "Glass Animals")
+
+            song_1 = Song.create(title: "Show Pony",
+                                 length: 255,
+                                 play_count: 12345,
+                                 artist: artist_1)
+            song_2 = Song.create(title: "Creatures in Heaven",
+                                 length: 221,
+                                 play_count: 2345,
+                                 artist: artist_1)
+
+            expect(artist_1.average_song_length).to eq(238)
+        end
+    end
 end
