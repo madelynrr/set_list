2e44a21d87da4ded2efcc797efe1cb12ec45bb7b
Madelyn Romero <m.romero013@gmail.com>
2025-03-21 14:34:41 -0600

    Create spec for Song#song_count

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Song, type: :model do
     describe "relationships" do
         it { should belong_to :artist }
     end
+
+    describe "class methods" do
+        it ".song_count" do
+            artist_1 = Artist.create(name: "Glass Animals")
+            artist_2 = Artist.create(name: "Taylor Swift")
+
+            song_1 = Song.create(title: "Show Pony",
+                                 length: 208,
+                                 play_count: 12345,
+                                 artist: artist_1)
+            song_2 = Song.create(title: "Peter",
+                                 length: 309,
+                                 play_count: 2345,
+                                 artist: artist_2)
+
+            expect(Song.song_count).to eq(2)
+        end
+    end
 end
