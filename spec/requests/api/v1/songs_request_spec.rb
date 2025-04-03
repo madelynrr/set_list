9c5a53c73fdd422f0ef154d5b641f7da70ac0ce5
Madelyn Romero <m.romero013@gmail.com>
2025-04-03 17:57:37 -0600

    Add request spec for API::V1::Songs

---
+++ spec/requests/api/v1/songs_request_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe "Songs API", type: :request do
+    it "sends a list of songs" do
+        artist = Artist.create(name: "Taylor Swift")
+        Song.create(title: "Peter", length: 220, play_count: 3, artist_id: artist.id)
+        Song.create(title: "Red", length: 295, play_count: 5, artist_id: artist.id)
+        Song.create(title: "Shake It Off", length: 219, play_count: 2, artist_id: artist.id)
+
+        get "/api/v1/songs"
+
+        expect(response).to be_successful
+
+        songs = JSON.parse(response.body, symbolize_names: true)
+
+        expect(songs.count).to eq(3)
+
+        songs.each do |song|
+            expect(song).to have_key(:id)
+            expect(song[:id]).to be_an(Integer)
+
+            expect(song).to have_key(:title)
+            expect(song[:title]).to be_an(String)
+
+            expect(song).to have_key(:length)
+            expect(song[:length]).to be_an(Integer)
+
+            expect(song).to have_key(:play_count)
+            expect(song[:play_count]).to be_an(Integer)
+
+            expect(song).to have_key(:artist_id)
+            expect(song[:artist_id]).to be_an(Integer)
+        end
+    end
+end
