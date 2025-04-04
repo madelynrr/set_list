9a1757c1dc7a504932b54eb4f90eeb9280845275
Madelyn Romero <m.romero013@gmail.com>
2025-04-03 18:02:06 -0600

    Add Api::V1::SongsController#show

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::SongsController < ApplicationController
     def index
         render json: Song.all
     end
+
+    def show
+        render json: Song.find(params[:id])
+    end
 end
