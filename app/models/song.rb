bd5f0d59197246eacb2699402887dffdaab5f95b
Madelyn Romero <m.romero013@gmail.com>
2025-04-07 14:54:14 -0600

    Add provate method to add popularity to a song before save

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ApplicationRecord
+    before_save { |song| song.popularity = calculate_popularity }
     validates :title, presence: true
     validates :length, presence: true
     validates :play_count, presence: true
@@ -7,5 +8,10 @@ class Song < ApplicationRecord
 
     def self.song_count
         all.count
+    end
+
+    private
+    def calculate_popularity
+        play_count > 5 ? "high" : "low"
     end
 end
